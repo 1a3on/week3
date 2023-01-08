16b677a38606c2ed48673faf9f14f0bcc0a1169d
As1Leo <swshenwen@gmail.com>
2023-01-08 22:59:05 +0800

    level 1

---
+++ js/login.js
@@ -0,0 +1,30 @@
+
+const rootPath = "https://vue3-course-api.hexschool.io/";
+const apiPath = "charizard";
+
+
+const app = Vue.createApp({
+    data(){         
+        return {
+            loginData:{
+                username : "",
+                password : ""
+            }
+        };
+    },
+
+    methods:{   
+        login(){        
+            console.log("------->",this.loginData);
+            axios.post(`${rootPath}V2/admin/signin`,this.loginData)
+                .then(res=>{
+                    const{token,expired} = res.data;      
+                    document.cookie = `hexToken=${token};expires=${new Date(expired)}`; //寫入token、有效期間timestamp轉日期
+                    window.location = 'products.html';
+                }).catch(err=>{alert(err.data.message);})
+        }
+    },
+
+})
+
+app.mount("#app");
